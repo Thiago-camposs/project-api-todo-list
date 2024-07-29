35a47c4188b037c91777ffaaa2df3408cfc41d44
Thiago Campos <thiago.campos715@gmail.com>
2024-07-29 17:15:50 -0300

    Initial commit

---
+++ src/main/java/thiago/project_api_todo_list/ProjectApiTodoListApplication.java
@@ -0,0 +1,13 @@
+package thiago.project_api_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thiago/project_api_todo_list/ProjectApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package thiago.project_api_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
